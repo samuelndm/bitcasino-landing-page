31c74a6c092ef1862b0585f4d11ad6abe9d14dc4
samuelndm <samuelnmoura@gmailcom>
2021-12-12 22:16:28 -0300

    build: add images to assets

---
+++ assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as bg } from "./bg.png";
+export { default as figure } from "./figure.png";
+export { default as icon } from "./icon.png";
+export { default as logo } from "./logo.png";
